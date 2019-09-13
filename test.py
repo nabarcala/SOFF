208b6d26988209d9bd1f3f6332631d4ace62f86c
JorgeMayo11789 <44385917+JorgeMayo11789@users.noreply.github.com>
2019-09-13 13:17:41 -0400

    Raspberry Pi Bluetooth script
    allows pi pi to connect to Bluetooth devices

---
+++ test.py
@@ -0,0 +1,37 @@
+from bluetooth import *
+
+
+server_sock=BluetoothSocket( RFCOMM )
+server_sock.bind(("",PORT_ANY))
+server_sock.listen(1)
+
+port = server_sock.getsockname()[1]
+
+uuid = "00001101-0000-1000-8000-00805F9B34FB"
+
+
+advertise_service( server_sock, "BTS",
+                   service_id = uuid,
+                   service_classes = [ uuid, SERIAL_PORT_CLASS ],
+                   profiles = [ SERIAL_PORT_PROFILE ], 
+                    )
+                   
+print("Waiting for connection on RFCOMM channel %d" % port)
+
+client_sock, client_info = server_sock.accept()
+print("Accepted connection from ", client_info)
+
+try:
+    while True:
+        data = client_sock.recv(1024)
+        if len(data) == 0: break
+        print("received [%s]" % data.decode("utf-8"))
+        
+except IOError:
+    pass
+
+print("Disconnected")
+
+client_sock.close()
+server_sock.close()
+print("All Closed"
