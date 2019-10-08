589abd52ff4fa89685e579abbbc8204384d39445
nabarcala <nabarcala@gmail.com>
2019-10-08 12:49:09 -0400

    Fix warnings

--- Fruit-Classification/Image-Classification/label_image.py
+++ Fruit-Classification/Image-Classification/label_image.py
@@ -25,7 +25,7 @@ import tensorflow as tf
 
 def load_graph(model_file):
   graph = tf.Graph()
-  graph_def = tf.GraphDef()
+  graph_def = tf.compat.v1.GraphDef()
 
   with open(model_file, "rb") as f:
     graph_def.ParseFromString(f.read())
@@ -42,7 +42,7 @@ def read_tensor_from_image_file(file_name,
                                 input_std=255):
   input_name = "file_reader"
   output_name = "normalized"
-  file_reader = tf.read_file(file_name, input_name)
+  file_reader = tf.io.read_file(file_name, input_name)
   if file_name.endswith(".png"):
     image_reader = tf.image.decode_png(
         file_reader, channels=3, name="png_reader")
@@ -56,9 +56,9 @@ def read_tensor_from_image_file(file_name,
         file_reader, channels=3, name="jpeg_reader")
   float_caster = tf.cast(image_reader, tf.float32)
   dims_expander = tf.expand_dims(float_caster, 0)
-  resized = tf.image.resize_bilinear(dims_expander, [input_height, input_width])
+  resized = tf.compat.v1.image.resize_bilinear(dims_expander, [input_height, input_width])
   normalized = tf.divide(tf.subtract(resized, [input_mean]), [input_std])
-  sess = tf.Session()
+  sess = tf.compat.v1.Session()
   result = sess.run(normalized)
 
   return result
@@ -66,14 +66,13 @@ def read_tensor_from_image_file(file_name,
 
 def load_labels(label_file):
   label = []
-  proto_as_ascii_lines = tf.gfile.GFile(label_file).readlines()
+  proto_as_ascii_lines = tf.io.gfile.GFile(label_file).readlines()
   for l in proto_as_ascii_lines:
     label.append(l.rstrip())
   return label
 
 
 if __name__ == "__main__":
-  # Defaults
   file_name = "tensorflow/examples/label_image/data/grace_hopper.jpg"
   model_file = \
     "tensorflow/examples/label_image/data/inception_v3_2016_08_28_frozen.pb"
@@ -85,7 +84,6 @@ if __name__ == "__main__":
   input_layer = "input"
   output_layer = "InceptionV3/Predictions/Reshape_1"
 
-  # Argument Parser
   parser = argparse.ArgumentParser()
   parser.add_argument("--image", help="image to be processed")
   parser.add_argument("--graph", help="graph/model to be executed")
@@ -130,22 +128,13 @@ if __name__ == "__main__":
   input_operation = graph.get_operation_by_name(input_name)
   output_operation = graph.get_operation_by_name(output_name)
 
-  # Get results based on all the labels
   with tf.Session(graph=graph) as sess:
     results = sess.run(output_operation.outputs[0], {
         input_operation.outputs[0]: t
     })
-  # Result values saved here to be printed later
   results = np.squeeze(results)
 
-  # Top fruit name = labels[0]
   top_k = results.argsort()[-5:][::-1]
   labels = load_labels(label_file)
-  # Print all fruits labels and their probability of being correct
   for i in top_k:
     print(labels[i], results[i])
-
-# End
-
-
-
