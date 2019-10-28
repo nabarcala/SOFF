2394508b35944067799be01f647ceee29f4f4541
nabarcala <nabarcala@gmail.com>
2019-10-28 11:47:12 -0400

    bt-compact script contains commands for bluetooth to work on the jetson nano

---
+++ bt-compat.sh
@@ -0,0 +1,24 @@
+!/bin/bash
+
+# Stop bluetooth first
+sudo /etc/init.d/bluetooth stop
+
+# Status check
+#/etc/init.d/bluetooth status
+
+# Run bluetooth in compatibility mode (don't forget ampersand, otherwise prompt won't turn up)
+sudo /usr/lib/bluetooth/bluetoothd --compat&
+
+# start bluetooth again
+sudo /etc/init.d/bluetooth start
+
+# again try sdpbrowse
+# sdptool browse local
+
+# check
+#systemctl status bluetooth.service
+
+# and change the permissions
+sudo chmod 777 /var/run/sdp
+
+
