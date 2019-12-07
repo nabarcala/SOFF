47eee51551f0ecaf12102ef25617302930ab988b
Natacha Barcala <nabarcala@gmail.com>
2019-12-07 12:00:51 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,14 +5,13 @@ cd Fruit-Classification/Image-Classification
 # Take a picture with the Raspberry Pi Camera V2
 python camera.py
 
-# Retrained Model
-#python3 label_image.py --graph=output_graph.pb --labels=output_labels.txt --input_layer=Placeholder --output_layer=final_result --image=fruit_img.jpg
-
 # Using NVIDIA's Inference: Classifying Images with ImageNet
 # --network flag is optional
 #python3 imagenet-console.py --network=googlenet fruit_img.jpg output_0.jpg  
 
-# Using retrained 
-python imagenet-console.py --model=fruit/resnet18.onnx --input_blob=input_0 --output_blob=output_0 --labels=fruit/labels.txt fruit_img.jpg fruit_img_output.png
+# Retrained Model:
 
+#python imagenet-console.py --model=fruit/resnet18.onnx --input_blob=input_0 --output_blob=output_0 --labels=fruit/labels.txt fruit_img.jpg fruit_img_output.png
 
+# Apple, Oranges
+python3 imagenet-console.py --model=images/resnet182.onnx --input_blob=input_0 --output_blob=output_0 --labels=images/temp.txt fruit_img.jpg fruit_img_output.png
